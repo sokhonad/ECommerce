50eea833f25003462f98969473dbf07973060ae5
sokhona <diaksokhona1993@gmail.com>
2022-03-06 11:28:44 +0100

    Création des contrôleurs

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+#[Route('/product', name: 'product_')]
+class ProductController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'details')]
+    public function details(Product $product): Response
+    {
+        return $this->render('product/details.html.twig', compact('product'));
+    }
+}
